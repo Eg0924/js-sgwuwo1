295bb5c32c6bced1b51cc992eaf47d128caf37ca
Eg0924 <eddytiger17@yahoo.com>
2021-03-30 09:27:49 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let strNum = " ";
+
+
+for (let i = 0; i < numbers.length; i++) {
+    strNum = strNum + numbers[i];
+}
+
+
+console.log(strNum);
+console.log(typeof strNum);
